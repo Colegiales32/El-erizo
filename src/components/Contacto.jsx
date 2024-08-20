448993e6c3b6f1f5c1c5338823d451e1caf11a76
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-08-20 20:02:27 -0300

    correcion logos redes contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -42,19 +42,19 @@ const Contacto = () => {
             <h3 className='text-[1.6rem] lg:text-[1.4rem] mx-auto text-oscuro'>Medios de contacto</h3>
             <div className='flex mx-auto gap-[1.5rem] py-[1rem] flex-col justify-between'>
                 <div className='flex flex-row justify-start gap-[1rem]'>
-                    <img className='object-cover' src="./public//mail.png" alt="Mail" />
+                    <img className='object-cover' src="/mail.png" alt="Mail" />
                     <p className='text-base text-claro font-light'>elerizoespaciocreativo@gmail.com</p>
                 </div>
                 <div className='flex flex-row justify-start gap-[1rem]'>
-                    <img src="./public/instagram.png" alt="Instagram" />
+                    <img src="/instagram.png" alt="Instagram" />
                     <p className='text-base text-claro font-light'>@elerizoespaciocreatiivo</p>
                 </div>
                 <div className='flex flex-row justify-start gap-[1rem]'>
-                    <img src="./public/wp.png" alt="WhatsApp" />
+                    <img src="/wp.png" alt="WhatsApp" />
                     <p className='text-base text-claro font-light'>XX-XXXX-XXXX</p>
                 </div>
                 <div className='flex flex-row justify-start gap-[1rem]'>
-                    <img src="./public/ubicacion.png" alt="Ubicacion" />
+                    <img src="/ubicacion.png" alt="Ubicacion" />
                     <p className='text-base text-claro font-light'>Cabrera 5356, Palermo Hollywood.</p>
                 </div>
                
