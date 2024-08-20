8e3f07d740067a5f5e57953d25f65a181a7a414d
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-08-20 19:13:52 -0300

    intento de correccion imagen curso

--- src/components/Cursos.jsx
+++ src/components/Cursos.jsx
@@ -4,7 +4,7 @@ const Cursos = () => {
   return (
     <div class="h-auto selection:bg-celeste mt-5 mb-16 md:mb-24 md:mt-8 md:h-min-[14rem] lg:h-[14rem] font-calibri p-[1rem] bg-[#FCDED3] rounded-[10px] mx-[1.2rem] md:mx-[6rem] lg:w-[46rem] lg:mx-auto flex flex-col md:flex-row gap-5 justify-between items-center sombra">
         <div className="w-full h-full md:max-w-[14rem] md:min-w-[14rem]  overflow-hidden rounded-[10px]">
-            <img className=' aspect-square w-full object-cover' src="./public/imagen-curso.jpg" alt="imagen del curso" />
+            <img className=' aspect-square w-full object-cover' src="/imagen-curso.jpg" alt="imagen del curso" />
         </div>
     <div class="flex flex-col justify-between items-start">
         <h2 class="self-stretch h-[1.875rem] text-[#272727] text-[1.375rem] font-bold break-words">Alfareria</h2>
