2e3f8be7051fc44b225035a00ff70dbbbaef3d99
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-08-22 19:29:32 -0300

    mejora accesibilidad

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = ({imagen,titulo,texto
 }) => {
   return (
     <div className="w-[21rem] h-[22rem] flex-col overflow-hidden justify-start items-center rounded-[10px] inline-flex relative sombra bg-[#FCDED3]">
-            <img className="w-full object-cover h-[10rem]" src={imagen} />
+            <img className="w-full object-cover h-[10rem]" src={imagen} alt='imagen del evento' />
             <div className="mt-[1rem] mx-[1.8rem] flex-col justify-center items-center gap-1 inline-flex">
                 <h4 className="self-stretch text-center text-[#272727] text-[1rem] font-bold font-['Calibri']">{titulo}</h4>
                 <p className="self-stretch text-[#474747] text-base font-light font-['Calibri']">{texto}</p>
