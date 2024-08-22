2d82da51fa7a5b627b43b975e72bfb9e1d1ad1eb
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-08-22 14:23:07 -0300

    intento de arreglarla

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        calibri: ['Calibri', 'sans-serif']
+      fontFamily :{
+        calibri : ['Calibri', 'sans-serif' ]
       },
-      colors: {
-        'oscuro': '#272727',
-        'claro': '#474747',
-        'rosa-claro': '#FCDED3',
-        'celeste': '#54AFA6'
+      colors : {
+        'oscuro' : '#272727',
+        'claro' : '#474747',
+        'rosa-claro' : '#FCDED3',
+        'celeste' : '#54AFA6'
       },
-      backgroundImage: {
-        'fondo': "url('/public/fondo.png)",
+      backgroundImage : {
+        'fondo' : "url('/public/fondo.png)",
       }
     },
   },
   plugins: [],
 }
+
