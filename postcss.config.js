ff7ea373b5a82292fadb8380b79a44e0efcf629f
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-09-16 16:42:15 -0300

    preloads

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Solo activa PurgeCSS en producción
+    ...(process.env.NODE_ENV === 'production'
+      ? [require('@fullhuman/postcss-purgecss')({
+          content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+          // Opcional: Puedes añadir configuraciones adicionales aquí si es necesario
+        })]
+      : [])
+  ],
 }
