166a803f473fb642a34d0d4c6d6d71ba0e822812
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-09-18 17:36:40 -0300

    texto intro

--- src/components/Introduccion.jsx
+++ src/components/Introduccion.jsx
@@ -4,8 +4,8 @@ const Introduccion = () => {
   return (
     <div className='pt-20 fade-in-up'>
        <div loading='lazy' rel='preload' className="w-auto h-[43rem] font-intro md:h-[30rem] mx-[1.2rem] md:mx-[7.2rem] my-6 md:my-10 relative bg-[url('/Introduccion2.webp')] bg-cover bg-bottom sombra rounded-[10px] 2xl:mx-[22rem] ">
-         <p className="lg:left-[2rem] sombratexto lg:top-[5rem] pt-[3.20rem] md:pt-0 text-center lg:absolute mx-4 text-white text-[2.6rem] ">Vení a aprender todo sobre este hermoso oficio!</p>
-         <p className="lg:left-[6.4rem] lg:top-[11rem] sombratexto lg:absolute text-center text-white mx-4 text-[2.5rem]  md:my-0 mt-[4.5rem]">Animate a traer tus propias ideas sobre lo que queres crear!</p>
+         <p className="lg:left-[2rem] sombratexto lg:top-[5rem] pt-[1rem] md:pt-0 text-center lg:absolute mx-4 text-white text-[2.6rem] ">Vení a aprender todo sobre este hermoso oficio!</p>
+         <p className="lg:left-[6.4rem] lg:top-[11rem] sombratexto lg:absolute text-center text-white mx-4 text-[2.5rem]  lg:my-0 md:mt-[6rem] mt-[9.5rem]">Animate a traer tus propias ideas sobre lo que queres crear!</p>
          <p className="right-[2rem] bottom-[1rem] sombratexto absolute text-white md:text-[3rem] text-[2rem]  leading-9">¡Te esperamos!</p>
 </div>
 
